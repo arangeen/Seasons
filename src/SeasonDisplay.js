63f1ee118020e51f7eae3d848fe860b42f55d68b
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-08-13 16:07:45 -0700

    ternary expressions in JSX

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -12,8 +12,13 @@ const SeasonDisplay = props => {
   //console.log(props.lat);
 
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(season);
-  return <div>Season Display</div>;
+  const text =
+    season === "winter" ? "Burr, it is chilly!" : "Lets hit the beach!";
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
