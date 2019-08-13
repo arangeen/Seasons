eb41694d3a8f855400b64283becc9ccdd0e7cc65
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-08-13 15:50:23 -0700

    intializing State differently

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, errorMessage: "" };
-  }
+  //different way to initilize the state instead of doing it inside the constructor
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
