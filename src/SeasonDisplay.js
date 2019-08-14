2eb76c1afece0f84441c04d6fda7fb0e38113fd5
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-08-13 17:17:19 -0700

    added some styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
 const seasonConfig = {
@@ -20,16 +21,14 @@ const getSeason = (lat, month) => {
 };
 
 const SeasonDisplay = props => {
-  //console.log(props.lat);
-
   const season = getSeason(props.lat, new Date().getMonth());
-  const { text, iconName } = seasonConfig[season]; // { text , iconName}
+  const { text, iconName } = seasonConfig[season];
 
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    <div className={`seaosn-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 };
