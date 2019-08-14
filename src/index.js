6a297ba9d47d2be2fd42fdbd94e606aa9a87edbe
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-08-13 17:25:22 -0700

    showing a loading spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   //different way to initilize the state instead of doing it inside the constructor
@@ -23,7 +24,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return <Spinner />;
   }
 }
 
